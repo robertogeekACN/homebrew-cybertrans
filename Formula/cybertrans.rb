98accdbd39d85e3542375f75304efe5ba9e93a6a
Roberto Romero <roberto.romero.perez+accent@accenture.com>
2026-06-24 19:05:40 +0200

    fix: use raw.githubusercontent.com URL to avoid corporate SSO redirect

--- Formula/cybertrans.rb
+++ Formula/cybertrans.rb
@@ -1,7 +1,7 @@
 class Cybertrans < Formula
   desc "Batch-transcode videos to web-optimised H.264/H.265 in-place with cyberpunk UI"
-  homepage "https://github.com/AccentureCodeFoundry/314899_Cybertranscoding"
-  url "https://github.com/AccentureCodeFoundry/314899_Cybertranscoding/releases/download/v1.0.0/cybertrans.pyz"
+  homepage "https://github.com/robertogeekACN/CyberTranscoder"
+  url "https://raw.githubusercontent.com/robertogeekACN/CyberTranscoder/dist/cybertrans.pyz"
   sha256 "d9f15e9fdd4994b65ef60cb4873101a07f5782988c40af0505967f1314a0f910"
   license "MIT"
 
